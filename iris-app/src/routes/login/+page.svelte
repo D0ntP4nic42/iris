836a84fc673f6d62e72c443461a29cbc34a6705f
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-09-03 17:54:45 -0300

    Componente de Login adicionado.
    Foi adicionada a rota de login do aplicativo.
    
    Criado o componente de login, que ainda falta ser feita a implementação do código respectivo que realizará o envio dos dados com a API do backend

---
+++ iris-app/src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Login from './Login.svelte'
+</script>
+
+<div class="background h-100 w-100">
+    <div class="container w-75 h-75">
+        <h1 class="text-center roboto-black title">Íris</h1>
+        <Login></Login>
+    </div>
+</div>
+
+<style>
+    .background {
+        background-image: url('/img/cedlan.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .title {
+        color: #1351B4;
+    }
+</style>

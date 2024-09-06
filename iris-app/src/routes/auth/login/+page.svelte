0cc31452d7d6d276bcafe52f667e0e6fc9fde116
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-09-06 17:08:50 -0300

    Aplicados os componentes de Input no formulário de Login

---
+++ iris-app/src/routes/auth/login/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Login from '$lib/components/form/Login.svelte';
+</script>
+
+<div class="background">
+	<div class="container w-75 h-75">
+		<Login/>
+	</div>
+</div>
+
+<style>
+	.background {
+		background-color: #003c71;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.container {
+		position: relative;
+		z-index: 2;
+	}
+</style>

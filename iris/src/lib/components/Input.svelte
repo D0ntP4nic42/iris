a10a82ff6d57e1e184ba61ec67b576adf3865277
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-09-19 20:43:09 -0300

    Corrigindo o icon dos inputs

--- iris/src/lib/components/Input.svelte
+++ iris/src/lib/components/Input.svelte
@@ -9,16 +9,10 @@
     export let maxlength: number | undefined = undefined;
     export let autocomplete: string = 'on';
     export let errorMessage: object | undefined = undefined;
-
 </script>
 
 <div>
     <label class="input input-bordered flex items-center gap-2">
-        <svg
-                class="h-4 w-4 opacity-70"
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-        </svg>
         <input
                 {name}
                 {...{type}}

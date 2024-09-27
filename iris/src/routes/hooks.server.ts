500d7b4488f38071124bc12733c8b1d7a22b0130
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-09-26 22:21:57 -0300

    HOOKS

--- iris/src/routes/hooks.server.ts
+++ iris/src/routes/hooks.server.ts
@@ -1,5 +1,5 @@
 import type {Handle} from "@sveltejs/kit";
 
 export const handle = (async ({ event, resolve }) => {
-    return await resolve(event);
+    return resolve(event);
 }) satisfies Handle;
--- iris/src/routes/protected/professor/+page.svelte
+++ iris/src/routes/protected/professor/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
     import AlunosTable from "$lib/components/AlunosTable.svelte";
+    import { page } from '$app/stores';
 </script>
 <h1>Olá</h1>

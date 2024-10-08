0a2a712919208478b9a9226696f80fbc27b12254
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-10-08 12:10:25 -0300

    adicionando botão que permite mudar a visibilidade da senha.

--- src/routes/protected/coordenador/register/professor/+page.svelte
+++ src/routes/protected/coordenador/register/professor/+page.svelte
@@ -34,6 +34,7 @@
     };
 
     let showModal = false;
+    let showPassword = false;
     let resolveSubmit: (value: boolean) => void;
 
     function confirmSubmit() {
@@ -49,6 +50,10 @@
     $: if (showModal) {
         const dialog = document.getElementById('modal_confirm') as HTMLDialogElement;
         dialog.showModal();
+    }
+
+    function togglePasswordVisibility() {
+        showPassword = !showPassword;
     }
 </script>
 
@@ -111,21 +116,45 @@
             <div class="m-3">
                 <label class="label-text" for="Senha">Senha</label>
                 <label class="input input-bordered flex items-center gap-2">
-                    <input
-                            name="password"
-                            type="password"
-                            class="grow"
-                            placeholder="Senha"
-                            spellcheck="false"
-                            autocomplete="off"
-                            bind:value={$form.password}
-                    />
-                    <svg
-                            class="h-4 w-4 opacity-70"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-                    </svg>
+                    {#if showPassword}
+                        <input
+                                name="password"
+                                type="text"
+                                class="grow"
+                                placeholder="Senha"
+                                spellcheck="false"
+                                autocomplete="off"
+                                bind:value={$form.password}
+                        />
+                    {:else}
+                        <input
+                                name="password"
+                                type="password"
+                                class="grow"
+                                placeholder="Senha"
+                                spellcheck="false"
+                                autocomplete="off"
+                                bind:value={$form.password}
+                        />
+                    {/if}
+                    <button type="button" on:click={togglePasswordVisibility}>
+                        {#if showPassword}
+                            <svg
+                                    class="h-4 w-4 opacity-70"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                            </svg>
+                        {:else}
+                            <svg
+                                    class="h-4 w-4 opacity-70"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                        {/if}
+                    </button>
                 </label>
+
                 {#if $errors.password}
                     <p class="text-error text-sm m-0 ml-1 mt-2">{$errors.password}</p>
                 {/if}
@@ -157,7 +186,7 @@
         <dialog id="modal_confirm" class="modal">
             <div class="modal-box">
                 <h3 class="text-lg font-bold">Aviso:</h3>
-                <p class="py-4">Ao confirmar esse diálogo, as alterações serão confirmadas, verifique se os dados estão realmente corretos antes de confirmar.</p>
+                <p class="py-4">Ao confirmar esse diálogo, as alterações serão enviadas, verifique se os dados estão realmente corretos.</p>
                 <div class="modal-action">
                     <form method="dialog">
                         <button class="btn" on:click={confirmSubmit}>Confirmar</button>
